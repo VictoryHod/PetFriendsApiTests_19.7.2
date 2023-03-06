d5763f0f05f4bc2492584ce760fee42a8533af19
17.9.1 <khodosevich.vika@bk.ru>
2023-03-06 17:16:15 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+valid_email = 'kusya040593@mail.ru'
+valid_password = 'Persiki'
+invalid_email = 'kusya0000@mail.ru'
+invalid_password = '123qwerty'
+empty_email = ""
+empty_password = ""
+empty_auth_key = {'key': ''}
+incorrect_auth_key = {'key': '000000000000000000000000000000000000000000000'}
